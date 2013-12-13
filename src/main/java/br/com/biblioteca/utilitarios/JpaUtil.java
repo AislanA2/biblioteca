78e17c8d56f931a1af10a2a118088ba6a312c7a3
Paulo Benety <pbenety@gmail.com>
2013-12-13 19:40:57 -0300

    Adição do banco HSQLDB e utilitário para recuperar entityManager

---
+++ src/main/java/br/com/biblioteca/utilitarios/JpaUtil.java
@@ -0,0 +1,57 @@
+package br.com.biblioteca.utilitarios;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaUtil {
+	
+	private static final String PERSISTENCE_UNIT_NAME = "biblioteca";
+	
+	private static ThreadLocal<EntityManager> manager = new ThreadLocal<EntityManager>();
+
+	private static EntityManagerFactory factory;
+	
+	private JpaUtil(){}
+	
+	public static EntityManager getEntityManager(){
+		
+		if (JpaUtil.factory == null) {
+			
+			JpaUtil.factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+			
+		}
+		
+		EntityManager em = JpaUtil.manager.get();
+		
+		if (em == null || !em.isOpen()) {
+			em = JpaUtil.factory.createEntityManager();
+			JpaUtil.manager.set(em);
+		}
+		
+		return em;
+		
+	}
+	
+	public static void closeEntityManager() {
+		
+		EntityManager em = JpaUtil.manager.get();
+		
+		if (em != null) {
+			EntityTransaction tx = em.getTransaction();
+			if (tx.isActive()) { 
+				tx.commit();
+			}
+			em.close();
+			JpaUtil.manager.set(null);
+		}
+		
+	}
+	
+	public static void closeEntityManagerFactory(){
+		closeEntityManager();
+		factory.close();
+	}
+
+}
