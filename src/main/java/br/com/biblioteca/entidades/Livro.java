0c54d72d2f6102e34e45a5085073269f153b5f8e
Paulo Benety <pbenety@gmail.com>
2013-12-13 19:55:04 -0300

    Adição da entidade Livro

---
+++ src/main/java/br/com/biblioteca/entidades/Livro.java
@@ -0,0 +1,66 @@
+package br.com.biblioteca.entidades;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Livro {
+
+	@Id @GeneratedValue
+	private Long id;
+	private String titulo;
+	private String autor;
+	private String categoria;
+	private Date dataPublicacao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public Date getDataPublicacao() {
+		return dataPublicacao;
+	}
+
+	public void setDataPublicacao(Date dataPublicacao) {
+		this.dataPublicacao = dataPublicacao;
+	}
+	
+	@Override
+	public String toString() {
+		return "Livro [id=" + id + ", titulo=" + titulo + ", autor=" + autor
+				+ ", categoria=" + categoria + ", dataPublicacao="
+				+ dataPublicacao + "]";
+	}
+
+}
